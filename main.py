250eaed5c1b7ae887ad4c5db1c88eb38a7f7ffcf
Abasifreke Isaac <79318104+Abas-dev@users.noreply.github.com>
2024-09-03 18:15:07 +0100

    added new things from path and query parameters

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from typing import Optional
 
 app = FastAPI()
 
@@ -6,6 +7,19 @@ app = FastAPI()
 async def read_root():
     return {'message':'hello world'}
 
-@app.get('/greet/{name}')
-async def greet(name:str) -> dict:
+@app.get('/path_greet/{name}')
+async def greet_name(name:str) -> dict:
     return {'message':f'hello {name}'}
+
+@app.get('/query_greet')
+async def greet_name_01(name:str) -> dict:
+    return {'message':f'hello {name}'}
+
+@app.get('/greet/{name}')
+async def greet_name_02(name:str, age:int) -> dict:
+    return {'message':f'hello {name}, you are {age} years old'}
+
+'''  this is to make the path parameter optional using python Optional class from typing '''
+@app.get('/optional_greet')
+async def greet_name_02(name:Optional[str]='User', age:int=0) -> dict:
+    return {'message':f'hello {name}, you are {age} years old'}
