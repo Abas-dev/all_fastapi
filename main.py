0c55b46ea7661f898e0a9e0fdfbd1f2ef96b040a
Abasifreke Isaac <79318104+Abas-dev@users.noreply.github.com>
2024-09-04 06:33:18 +0100

    added route to get request headers

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Header
 from typing import Optional
+
+from pydantic import BaseModel
 
 app = FastAPI()
 
@@ -21,5 +23,34 @@ async def greet_name_02(name:str, age:int) -> dict:
 
 '''  this is to make the path parameter optional using python Optional class from typing '''
 @app.get('/optional_greet')
-async def greet_name_02(name:Optional[str]='User', age:int=0) -> dict:
+async def greet_name_03(name:Optional[str]='User', age:int=0) -> dict:
     return {'message':f'hello {name}, you are {age} years old'}
+
+
+'''i am starting to use post request from here to be sendig data from the request body'''
+
+class BookCreateModel(BaseModel):
+    title : str 
+    author : str
+
+@app.post('/create_book')
+async def create_book(book_data:BookCreateModel):
+    return {
+        'title': book_data.title,
+        'author': book_data.author
+    }
+
+@app.get('/get_headers', status_code=200)
+async def get_headers(accept:str = Header(None), 
+                      content_type:str = Header(None), 
+                      user_agent:str = Header(None),
+                      host:str = Header(None),
+):
+    request_headers = {}
+
+    request_headers['Accept'] = accept
+    request_headers['Content_Type'] = content_type
+    request_headers['User-Agent'] = user_agent
+    request_headers['Host'] = host
+
+    return request_headers 
