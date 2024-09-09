1bc1f543cb1162f2cedbc3bdcb06204a675093e8
Abasifreke Isaac <79318104+Abas-dev@users.noreply.github.com>
2024-09-09 07:42:36 +0100

    added a simple db connection

--- src/__init__.py
+++ src/__init__.py
@@ -1,12 +1,22 @@
 from fastapi import FastAPI
 from src.books.routes import book_router
+from contextlib import asynccontextmanager
+from src.db.main import init_db
+
+@asynccontextmanager
+async def life_span(app:FastAPI):
+    print(f"server is running and is now live ....")
+    await init_db()
+    yield
+    print(f"server has been stopped ....")
 
 version = "v1"
 
 app = FastAPI(
     version=version,
     title="Book base",
-    description="A simple book review service"
+    description="A simple book review service",
+    lifespan=life_span 
 )
 
 app.include_router(book_router, prefix=f'/api/{version}/books', tags="books")
